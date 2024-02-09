d1358c260d67d04e6a04837d1918aafca03c0069
Guilherme Paterlini <guilhermepaterlini@gmail.com>
2024-02-08 22:26:57 -0400

    adicionado favicon e logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import logo from "./assets/logo-nlw-expert.svg";
+
 export function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <img src={logo} />;
+      <input type="text" placeholder="Busque em suas notas..." />
+    </div>
+  );
 }
