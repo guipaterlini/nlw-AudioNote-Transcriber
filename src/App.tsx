30123306af6a4aec7970c251fa3bcc8324df67a9
Guilherme Paterlini <guilhermepaterlini@gmail.com>
2024-02-09 21:17:39 -0400

    Criado a base para os card e adicionado o espaçamento

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import logo from "./assets/logo-nlw-expert.svg";
 
 export function App() {
   return (
-    <div className="mx-auto max-w-6xl my-12 ">
+    <div className="mx-auto max-w-6xl my-12 space-y-6">
       <img src={logo} alt="NLW Expert" />
+
       <form className="w-full">
         <input
           type="text"
@@ -11,6 +12,20 @@ export function App() {
           className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
         />
       </form>
+
+      <div className="h-px bg-slate-700" />
+
+      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+        <div className="rounded-md bg-slate-700">
+          
+          </div>
+        <div className="rounded-md bg-slate-800">
+          
+          </div>
+        <div className="rounded-md bg-slate-800">
+          
+          </div>
+      </div>
     </div>
   );
 }
