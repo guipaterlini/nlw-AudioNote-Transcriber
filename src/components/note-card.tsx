96a14f8c43ec3643de5a1c7703c55000cdc024ce
Guilherme Paterlini <guilhermepaterlini@gmail.com>
2024-02-10 15:46:50 -0400

    adiciona propriedades ao note card e instala radix dialog

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,16 +1,17 @@
-export function NoteCard() {
+interface NoteCardProps {
+  note: {
+    date: Date;
+    content: string;
+  };
+}
+
+export function NoteCard({ note }: NoteCardProps) {
   return (
-    <button className="rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
-      <span className="text-sm font-medium text-slate-300">há 2 dias </span>
-      <p className="text-sm leading-6 text-slate-400">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
-        possimus corporis saepe molestiae exercitationem autem, amet adipisci ut
-        eum beatae eligendi aut neque, sint natus, officiis cupiditate!
-        Reiciendis, itaque asperiores? Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Eveniet possimus corporis saepe molestiae
-        exercitationem autem, amet adipisci ut eum beatae eligendi aut neque,
-        sint natus, officiis cupiditate! Reiciendis, itaque asperiores?
-      </p>
+    <button className="rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
+      <span className="text-sm font-medium text-slate-300">
+        {note.date.toISOString()}
+      </span>
+      <p className="text-sm leading-6 text-slate-400">{note.content}</p>
 
       <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
     </button>
