58e62aaab5c9bf9f4b1c0632e0e43f47d3018d13
Guilherme Paterlini <guilhermepaterlini@gmail.com>
2024-02-09 21:57:59 -0400

    adicionado uma gradiente para fazer sombra nos cards

--- src/App.tsx
+++ src/App.tsx
@@ -16,15 +16,55 @@ export function App() {
       <div className="h-px bg-slate-700" />
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
-        <div className="rounded-md bg-slate-700">
-          
-          </div>
-        <div className="rounded-md bg-slate-800">
-          
-          </div>
-        <div className="rounded-md bg-slate-800">
-          
-          </div>
+        <div className="rounded-md bg-slate-700 p-5 space-y-3">
+          <span className="text-sm font-medium text-slate-200">
+            Adicionar nota
+          </span>
+          <p className="text-sm leading-6 text-slate-400">
+            Grave uma nota em áudio que será convertida para texto
+            automaticamente.
+          </p>
+        </div>
+
+        <div className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative">
+          <span className="text-sm font-medium text-slate-300">há 2 dias </span>
+          <p className="text-sm leading-6 text-slate-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
+            possimus corporis saepe molestiae exercitationem autem, amet
+            adipisci ut eum beatae eligendi aut neque, sint natus, officiis
+            cupiditate! Reiciendis, itaque asperiores? Lorem ipsum dolor sit
+            amet consectetur adipisicing elit. Eveniet possimus corporis saepe
+            molestiae exercitationem autem, amet adipisci ut eum beatae eligendi
+            aut neque, sint natus, officiis cupiditate! Reiciendis, itaque
+            asperiores?
+          </p>
+
+          <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0" />
+        </div>
+
+        <div className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative">
+          <span className="text-sm font-medium text-slate-300">há 4 dias</span>
+          <p className="text-sm leading-6 text-slate-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+            dolorum illum quis neque sunt sapiente, voluptatem ipsa id
+            repellendus natus at eius eum, esse rerum adipisci reiciendis sint
+            et est?
+          </p>
+
+          <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0" />
+        </div>
+
+        <div className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative">
+          <span className="text-sm font-medium text-slate-300">há 7 dias</span>
+          <p className="text-sm leading-6 text-slate-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+            dolorum illum quis neque sunt sapiente, voluptatem ipsa id
+            repellendus natus at eius eum, esse rerum adipisci reiciendis sint
+            et est?
+          </p>
+
+          <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0" />
+        </div>
       </div>
     </div>
   );
