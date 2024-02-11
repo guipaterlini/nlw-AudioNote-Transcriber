d9f3de59aebb78a9f2ee0a69ac9e94719f2a413a
Guilherme Paterlini <guilhermepaterlini@gmail.com>
2024-02-11 15:21:04 -0400

    adiciona o funcionamento de reconhecimento de voz e transcrição para salvar novas notas

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -40,8 +40,6 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
   }
 
   function handleStartRecording() {
-    setIsRecording(true);
-
     const isSpeechRecognitionAPIAvailable =
       "SpeechRecognition" in window || "webkitSpeechRecognition" in window;
 
@@ -49,6 +47,33 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
       alert("Infelizmente seu navegador não suporta a API de gravação!");
       return;
     }
+
+    setIsRecording(true);
+    setShouldShowOnboarding(false);
+
+    const SpeechRecognitionAPI =
+      window.SpeechRecognition || window.webkitSpeechRecognition;
+
+    const speechRecognition = new SpeechRecognitionAPI();
+
+    speechRecognition.lang = "pt-BR";
+    speechRecognition.continuous = true;
+    speechRecognition.maxAlternatives = 1;
+    speechRecognition.interimResults = true;
+
+    speechRecognition.onresult = (event) => {
+      const transcription = Array.from(event.results).reduce((text, result) => {
+        return text.concat(result[0].transcript);
+      }, "");
+
+      setContent(transcription);
+    };
+
+    speechRecognition.onerror = (event) => {
+      console.error(event);
+    };
+
+    speechRecognition.start();
   }
 
   function handleStopRecording() {
